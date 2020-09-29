0d07879f36cd265e91c47a88c1a9b8fcaeb2750a
Charlie Cidral <charliesamoel@gmail.com>
2020-09-29 15:06:49 -0300

    electronic ballot box

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo:'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Paulo',
+                partido: 'ABC',
+                fotos:[
+                    {url:'avatar1.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Cezar',
+                partido: 'DEF',
+                fotos:[
+                    {url:'avatar2.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo:'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Gustavo',
+                partido: 'ABC',
+                vice: 'Ricardo',
+                fotos:[
+                    {url:'avatar3.png', legenda: 'Prefeito'},
+                    {url:'avatar4.png', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Gabriel',
+                partido: 'DEF',
+                vice: 'Junior',
+                fotos:[
+                    {url:'avatar5.png', legenda: 'Prefeito'},
+                    {url:'avatar6.png', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+        ]
+    }
+];
